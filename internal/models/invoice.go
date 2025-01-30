41825dbe223c9f4ac38a606ffbf2f93876b4ad17
neghi14 <sambasseynengimote@gmail.com>
2025-01-30 22:39:07 +0100

    update: updated models

--- internal/models/invoice.go
+++ internal/models/invoice.go
@@ -1,19 +1,28 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 var (
-	InvDraft         string = "DRAFT"
-	InvOpen          string = "OPEN"
-	InvPaid          string = "PAID"
-	InvVoid          string = "VOID"
-	InvUncollectable string = "UNCOLLECTABLE"
+	InvDraft     string = "DRAFT"
+	InvIssued    string = "ISSUED"
+	InvPaid      string = "PAID"
+	InvExpired   string = "EXPIRED"
+	InvCancelled string = "CANCELLED"
 )
 
 type Invoice struct {
 	ID           uuid.UUID      `json:"id" db:"id,index,unique"`
 	CustomerID   uuid.UUID      `json:"customer_id" db:"customer_id,index"`
 	Amount       int64          `json:"amount" db:"amount"`
+	Description  string         `json:"description" db:"description"`
 	Status       string         `json:"status" db:"status"`
+	LastAttempt  time.Time      `json:"last_attempt" db:"last_attempt"`
+	AttemptCount int64          `json:"-" db:"attempt_count"`
+	PaidAt       time.Time      `json:"paid_at" db:"paid_at"`
+	ExpiresAt    time.Time      `json:"expires_at" db:"expires_at"`
 	Transactions []*Transaction `json:"transactions" db:"-"`
 }
--- internal/models/transaction.go
+++ internal/models/transaction.go
@@ -3,15 +3,15 @@ package models
 import "github.com/google/uuid"
 
 var (
-	TrxPending string = "PENDING"
-	TrxSuccess string = "SUCCESS"
-	TrxFailed  string = "FAILED"
+	TrxPending    string = "PENDING"
+	TrxSuccess    string = "SUCCESS"
+	TrxFailed     string = "FAILED"
+	TrxAbandonned string = "ABANDONED"
 )
 
 type Transaction struct {
 	ID        uuid.UUID `json:"id" db:"id,index,unique"`
 	InvoiceID uuid.UUID `json:"invoice_id" db:"invoice_id,index"`
-	Amount    int64     `json:"amount" db:"amount"`
 	Reference string    `json:"reference" db:"reference"`
 	Status    string    `json:"status" db:"status"`
 }
