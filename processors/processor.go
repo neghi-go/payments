3c16efd5e341b3d80e74cfafe0a318a962a14005
neghi14 <sambasseynengimote@gmail.com>
2025-01-30 22:38:49 +0100

    update: updated processor interface

--- processors/processor.go
+++ processors/processor.go
@@ -7,10 +7,20 @@ import (
 	"github.com/google/uuid"
 )
 
+type VerifyState int
+
+const (
+	Success VerifyState = iota + 1
+	Pending
+	Abandoned
+	Failed
+	Reversed
+)
+
 type Processor interface {
 	Init(ctx context.Context, email string, amount int64, reference string) (string, error)
 	Charge(ctx context.Context, email string, amount int64, card_token string, reference string) error
-	Verify(ctx context.Context, trx_id string) (bool, error)
+	Verify(ctx context.Context, trx_id string) (VerifyState, error)
 	Webhook(ctx context.Context, r *http.Request) error
 	Refund(ctx context.Context, trx_id uuid.UUID) error
 }
