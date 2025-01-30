19728e86cd72d97e91cada6cf08ccc945d6bde3e
neghi14 <sambasseynengimote@gmail.com>
2025-01-30 17:02:03 +0100

    update: set invoice states

--- internal/models/invoice.go
+++ internal/models/invoice.go
@@ -2,6 +2,14 @@ package models
 
 import "github.com/google/uuid"
 
+var (
+	InvDraft         string = "DRAFT"
+	InvOpen          string = "OPEN"
+	InvPaid          string = "PAID"
+	InvVoid          string = "VOID"
+	InvUncollectable string = "UNCOLLECTABLE"
+)
+
 type Invoice struct {
 	ID           uuid.UUID      `json:"id" db:"id,index,unique"`
 	CustomerID   uuid.UUID      `json:"customer_id" db:"customer_id,index"`
